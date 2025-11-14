022c22b1afb4a1e46f9d4a09088a661955ae56fb
Robert Forkel <xrotwang@googlemail.com>
2025-11-14 10:02:52 +0100

    treat categories properly

--- cldfbench_carneiro6.py
+++ cldfbench_carneiro6.py
@@ -118,6 +118,11 @@ class Dataset(DatasetWithSocieties):
         #Trait_ID_6th, Category, Trait_name, Trait_description
         for row in self.raw.read_csv('6th_edition_traits.Sheet1.csv', dicts=True):
             row = {k: v.strip() for k, v in row.items()}
+            if ',' in row['Category']:
+                # The D-PLACE CLDF model specifies ", " as separator for the content of the category
+                # column.
+                assert row['Category'] == 'Tools, Utensils, and Textiles'
+                row['Category'] = 'Tools and Utensils and Textiles'
             rid = row['Trait_ID_6th']
             # Trait_ID_6th,Category,Trait_description
             if row['Trait_ID_6th']:
